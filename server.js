43a1bb1ab9f1c5b1b0ecb983c9094dc935763ca3
Sheldrick <mrmayrant@yahoo.com>
2019-07-24 10:46:37 -0400

    created the foundation of the register route

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require("express");
-
+const knex = require("knex");
 const app = express();
 
-app.get("/", (req, res) => {
-	res.send("app connected");
+const db = knex({
+	client: "pg",
+	connection: {
+		host: "127.0.0.1",
+		user: "sheldrickmayrant",
+		password: "",
+		database: "saywhat"
+	}
 });
+
+// body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+	res.json({ msg: "App connected" });
+});
+
+app.use("/register", require("./routes/register"));
 
 const PORT = process.env.PORT || 3000;
 
