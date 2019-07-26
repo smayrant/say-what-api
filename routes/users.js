8f49ebe5b919f7f24fee95f9cbd7ddf946e005d7
Sheldrick <mrmayrant@yahoo.com>
2019-07-26 13:50:14 -0400

    added the ability to add a user to the DB with hard coded data

--- routes/users.js
+++ routes/users.js
@@ -1,25 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require("express-validator");
 const User = require("../models/Users");
 
-// router.post(
-// 	"/",
-// 	[
-// 		check("name", "Please add a name").not().isEmpty(),
-// 		check("email", "Please include a valid email address").isEmail(),
-// 		check("password", "Please ensure your password has 8 or more characters").isLength({ min: 8 })
-// 	],
-// 	(req, res) => {
-// 		const errors = validationResult(req);
-// 		// if ther are errors, send a 400 status and the array of errors
-// 		if (!errors.isEmpty()) {
-// 			return res.status(400).json({ errors: errors.array() });
-// 		} else {
-// 		}
-// 	}
-// );
-
+// route to get all users
 router.get("/", (req, res) => {
 	User.findAll()
 		.then(users => {
@@ -29,24 +12,34 @@ router.get("/", (req, res) => {
 		.catch(err => console.log(err));
 });
 
-router.get("/add", (req, res) => {
+// route to retrieve an individual user
+router.get("/:id", (req, res) => {
+	res.send(data.users[req.params.id]);
+});
+
+// route to register a user
+router.post("/", (req, res) => {
 	const data = {
-		name: "rick",
-		email: "user2@gmail.com",
-		post_entries: 2,
-		joined: new Date()
+		username: "tom",
+		email: "tom@gmail.com",
+		password: "123456",
+		bio: "Just browsing around",
+		posts: 1,
+		likes: 15
 	};
 
-	let { name, email, post_entries, joined } = data;
+	const { username, email, password, bio, posts, likes } = data;
 
 	User.create({
-		name,
+		username,
 		email,
-		post_entries,
-		joined
-	})
-		.then(gig => res.redirect("/"))
-		.catch(err => console.log(err));
+		password,
+		bio,
+		posts,
+		likes
+	}).then(newUser => {
+		console.log(newUser);
+	});
 });
 
 module.exports = router;
